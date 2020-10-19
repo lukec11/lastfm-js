81f9c427e84790db581d7d9d2ec444843d17d164
lukec11 <me@lukec.me>
2020-10-19 18:08:58 -0400

    hacky cors

--- api/index.js
+++ api/index.js
@@ -1,18 +1,26 @@
 const fetch = require('node-fetch');
 
 export default async function(req, res) {
-  const user = req.query.user;
-  // Fetch info from lastfm
-  let resp = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${user}&api_key=${process.env.API_KEY}&format=json&limit=1`)
-  resp = await resp.json();
+  // Hacky cors solution
+  if (req.method === 'OPTIONS') {
+		return res.status(200).end();
+  }
   
-  // Parse out song
-  let song = resp.recenttracks.track[0]
+	const user = req.query.user;
+	// Fetch info from lastfm
+	let resp = await fetch(
+		`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${user}&api_key=${process.env
+			.API_KEY}&format=json&limit=1`
+	);
+	resp = await resp.json();
 
-  // send formatted response
-  res.json({
-    song: song.name,
-    artist: song.artist['#text'],
-    url: song.url
-  })
+	// Parse out song
+	let song = resp.recenttracks.track[0];
+
+	// send formatted response
+	res.json({
+		song: song.name,
+		artist: song.artist['#text'],
+		url: song.url
+	});
 }
